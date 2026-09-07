ec694169bd581b253c2b679b5e22fec3ebdc02f3
lavipco <mvulam@gmail.com>
2026-09-07 14:10:03 +0700

    cap nhat

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *
  * Đổi nội dung app xong nhớ tăng VERSION để cache cũ bị dọn.
  */
-const VERSION = 'nktc-2026.09.06';   // PHAI khop APP_VERSION trong index.html
+const VERSION = 'nktc-2026.09.10';   // PHAI khop APP_VERSION trong index.html
 const SHELL = [
   './',
   './index.html',
